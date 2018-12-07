568a57c9633cecb6dd4cc8f73833255c5343c88c
alexpintea <alexpintea1991@gmail.com>
2018-12-07 20:18:33 +0200

    menu slick button

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -152,3 +152,31 @@ $(document).ready(function() {
 
 });
 
+
+
+/* Slick button hover*/
+let $cta = $('.cta');
+
+let createBtnsMarkup = () => {
+	$cta.each((i, btn) => {
+		let $t = $(btn);
+		let $btnText = $t.text();
+		let $splitText = $btnText.split("");
+
+		$t.html("").append(`
+				<span class="btn-text-parent">
+					<span class="wrap_text current-btn-text"></span>
+					<span class="wrap_text buildin-btn-text"></span>
+				</span>
+			`);
+
+		for (let i = 0; i < $splitText.length; i++) {
+			$t
+				.find(".wrap_text")
+				.append(`<span class="btn-letter">${$splitText[i]}</span>`);
+		}
+	});
+};
+
+
+window.onLoad = createBtnsMarkup();
