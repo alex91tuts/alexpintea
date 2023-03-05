ae0deea3c0c6a688ded2d421b7fd125b4dc81904
alex91tuts <41398466+alex91tuts@users.noreply.github.com>
2023-03-06 01:56:02 +0200

    test

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,81 +1,74 @@
-/*
-Template Name: Wrapkit Ui Kit
-Author: Themedesigner
-Email: niravjoshi87@gmail.com
-File: js
-*/
-$(function () {
-    "use strict";
-    // ============================================================== 
-    //This is for preloader
-    // ============================================================== 
-    $(function () {
-        $(".preloader").fadeOut();
+window.addEventListener("DOMContentLoaded", function() {
+  "use strict";
+  // ============================================================== 
+  //This is for preloader
+  // ============================================================== 
+  var preloader = document.querySelector(".preloader");
+  if (preloader !== null) {
+    preloader.style.display = 'none';
+  }
+  // ============================================================== 
+  //Tooltip
+  // ============================================================== 
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+  // ============================================================== 
+  //Popover
+  // ============================================================== 
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'));
+  popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
+  // ============================================================== 
+  // For mega menu
+  // ============================================================== 
+  document.querySelectorAll('.mega-dropdown').forEach(function(el) {
+    el.addEventListener('click', function(e) {
+      e.stopPropagation();
     });
-    // ============================================================== 
-    //Tooltip
-    // ============================================================== 
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
-    // ============================================================== 
-    //Popover
-    // ============================================================== 
-    $(function () {
-        $('[data-toggle="popover"]').popover()
-    })
-    // ============================================================== 
-    // For mega menu
-    // ============================================================== 
-    jQuery(document).on('click', '.mega-dropdown', function (e) {
-        e.stopPropagation()
+  });
+  document.querySelectorAll('.navbar-nav > .dropdown').forEach(function(el) {
+    el.addEventListener('click', function(e) {
+      e.stopPropagation();
     });
-     jQuery(document).on('click', '.navbar-nav > .dropdown', function(e) {
-         e.stopPropagation();
+  });
+  document.querySelectorAll('.dropdown-submenu').forEach(function(el) {
+    el.addEventListener('click', function() {
+      this.querySelector('.dropdown-menu').classList.toggle('show');
     });
-    $(".dropdown-submenu").click(function(){
-              $(".dropdown-submenu > .dropdown-menu").toggleClass("show");                     
-    });
-    // ============================================================== 
-    // Resize all elements
-    // ============================================================== 
-    $("body").trigger("resize");
-    // ============================================================== 
-    //Fix header while scroll
-    // ==============================================================  
-    var wind = $(window);
-    wind.on("load", function() {
-        var bodyScroll = wind.scrollTop(),
-            navbar = $(".topbar");
-        if (bodyScroll > 100) {
-            navbar.addClass("fixed-header animated slideInDown")
-        } else {
-            navbar.removeClass("fixed-header animated slideInDown")
-        }
-    });
-    $(window).scroll(function () {
-        if ($(window).scrollTop() >= 300) {
-            $('.topbar').addClass('fixed-header animated slideInDown');
-            $('.bt-top').addClass('visible');
-        } else {
-            $('.topbar').removeClass('fixed-header animated slideInDown');
-            $('.bt-top').removeClass('visible');
-        }
-    });
-    // ============================================================== 
-    // Animation initialized
-    // ============================================================== 
-		AOS.init();
-    // ============================================================== 
-    // Back to top
-    // ============================================================== 
-    $('.bt-top').on('click', function (e) {
-        e.preventDefault();
-        $('html,body').animate({
-            scrollTop: 0
-        }, 700);
-    });
-    
+  });
+  // ============================================================== 
+  // Resize all elements
+  // ============================================================== 
+  window.dispatchEvent(new Event('resize'));
+  // ============================================================== 
+  //Fix header while scroll
+  // ============================================================== 
+  var navbar = document.querySelector(".topbar");
+  window.addEventListener("scroll", function() {
+    var bodyScroll = window.scrollY;
+    if (bodyScroll > 100) {
+      navbar.classList.add("fixed-header", "animated", "slideInDown");
+    } else {
+      navbar.classList.remove("fixed-header", "animated", "slideInDown");
+    }
+    if (window.scrollY >= 300) {
+      document.querySelector('.topbar').classList.add('fixed-header', 'animated', 'slideInDown');
+      document.querySelector('.bt-top').classList.add('visible');
+    } else {
+      document.querySelector('.topbar').classList.remove('fixed-header', 'animated', 'slideInDown');
+      document.querySelector('.bt-top').classList.remove('visible');
+    }
+  });
+  // ============================================================== 
+  // Back to top
+  // ============================================================== 
+  document.querySelector('.bt-top').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
 });
 
 
@@ -84,9 +77,13 @@ $(function () {
 /*
  Sticky-kit v1.1.2 | WTFPL | Leaf Corcoran 2015 | http://leafo.net
 */
-(function(){var b,f;b=this.jQuery||window.jQuery;f=b(window);b.fn.stick_in_parent=function(d){var A,w,J,n,B,K,p,q,k,E,t;null==d&&(d={});t=d.sticky_class;B=d.inner_scrolling;E=d.recalc_every;k=d.parent;q=d.offset_top;p=d.spacer;w=d.bottoming;null==q&&(q=0);null==k&&(k=void 0);null==B&&(B=!0);null==t&&(t="is_stuck");A=b(document);null==w&&(w=!0);J=function(a,d,n,C,F,u,r,G){var v,H,m,D,I,c,g,x,y,z,h,l;if(!a.data("sticky_kit")){a.data("sticky_kit",!0);I=A.height();g=a.parent();null!=k&&(g=g.closest(k));
-if(!g.length)throw"failed to find stick parent";v=m=!1;(h=null!=p?p&&a.closest(p):b("<div />"))&&h.css("position",a.css("position"));x=function(){var c,f,e;if(!G&&(I=A.height(),c=parseInt(g.css("border-top-width"),10),f=parseInt(g.css("padding-top"),10),d=parseInt(g.css("padding-bottom"),10),n=g.offset().top+c+f,C=g.height(),m&&(v=m=!1,null==p&&(a.insertAfter(h),h.detach()),a.css({position:"",top:"",width:"",bottom:""}).removeClass(t),e=!0),F=a.offset().top-(parseInt(a.css("margin-top"),10)||0)-q,
-u=a.outerHeight(!0),r=a.css("float"),h&&h.css({width:a.outerWidth(!0),height:u,display:a.css("display"),"vertical-align":a.css("vertical-align"),"float":r}),e))return l()};x();if(u!==C)return D=void 0,c=q,z=E,l=function(){var b,l,e,k;if(!G&&(e=!1,null!=z&&(--z,0>=z&&(z=E,x(),e=!0)),e||A.height()===I||x(),e=f.scrollTop(),null!=D&&(l=e-D),D=e,m?(w&&(k=e+u+c>C+n,v&&!k&&(v=!1,a.css({position:"fixed",bottom:"",top:c}).trigger("sticky_kit:unbottom"))),e<F&&(m=!1,c=q,null==p&&("left"!==r&&"right"!==r||a.insertAfter(h),
-h.detach()),b={position:"",width:"",top:""},a.css(b).removeClass(t).trigger("sticky_kit:unstick")),B&&(b=f.height(),u+q>b&&!v&&(c-=l,c=Math.max(b-u,c),c=Math.min(q,c),m&&a.css({top:c+"px"})))):e>F&&(m=!0,b={position:"fixed",top:c},b.width="border-box"===a.css("box-sizing")?a.outerWidth()+"px":a.width()+"px",a.css(b).addClass(t),null==p&&(a.after(h),"left"!==r&&"right"!==r||h.append(a)),a.trigger("sticky_kit:stick")),m&&w&&(null==k&&(k=e+u+c>C+n),!v&&k)))return v=!0,"static"===g.css("position")&&g.css({position:"relative"}),
-a.css({position:"absolute",bottom:d,top:"auto"}).trigger("sticky_kit:bottom")},y=function(){x();return l()},H=function(){G=!0;f.off("touchmove",l);f.off("scroll",l);f.off("resize",y);b(document.body).off("sticky_kit:recalc",y);a.off("sticky_kit:detach",H);a.removeData("sticky_kit");a.css({position:"",bottom:"",top:"",width:""});g.position("position","");if(m)return null==p&&("left"!==r&&"right"!==r||a.insertAfter(h),h.remove()),a.removeClass(t)},f.on("touchmove",l),f.on("scroll",l),f.on("resize",
-y),b(document.body).on("sticky_kit:recalc",y),a.on("sticky_kit:detach",H),setTimeout(l,0)}};n=0;for(K=this.length;n<K;n++)d=this[n],J(b(d));return this}}).call(this);
+/*
+ Sticky-kit v1.1.3 | MIT | Leaf Corcoran 2015 | http://leafo.net
+*/
+(function(){var c,f;c=window.jQuery;f=c(window);c.fn.stick_in_parent=function(b){var A,w,J,n,B,K,p,q,L,k,E,t;null==b&&(b={});t=b.sticky_class;B=b.inner_scrolling;E=b.recalc_every;k=b.parent;q=b.offset_top;p=b.spacer;w=b.bottoming;null==q&&(q=0);null==k&&(k=void 0);null==B&&(B=!0);null==t&&(t="is_stuck");A=c(document);null==w&&(w=!0);L=function(a){var b;return window.getComputedStyle?(a=window.getComputedStyle(a[0]),b=parseFloat(a.getPropertyValue("width"))+parseFloat(a.getPropertyValue("margin-left"))+
+parseFloat(a.getPropertyValue("margin-right")),"border-box"!==a.getPropertyValue("box-sizing")&&(b+=parseFloat(a.getPropertyValue("border-left-width"))+parseFloat(a.getPropertyValue("border-right-width"))+parseFloat(a.getPropertyValue("padding-left"))+parseFloat(a.getPropertyValue("padding-right"))),b):a.outerWidth(!0)};J=function(a,b,n,C,F,u,r,G){var v,H,m,D,I,d,g,x,y,z,h,l;if(!a.data("sticky_kit")){a.data("sticky_kit",!0);I=A.height();g=a.parent();null!=k&&(g=g.closest(k));if(!g.length)throw"failed to find stick parent";
+v=m=!1;(h=null!=p?p&&a.closest(p):c("<div />"))&&h.css("position",a.css("position"));x=function(){var d,f,e;if(!G&&(I=A.height(),d=parseInt(g.css("border-top-width"),10),f=parseInt(g.css("padding-top"),10),b=parseInt(g.css("padding-bottom"),10),n=g.offset().top+d+f,C=g.height(),m&&(v=m=!1,null==p&&(a.insertAfter(h),h.detach()),a.css({position:"",top:"",width:"",bottom:""}).removeClass(t),e=!0),F=a.offset().top-(parseInt(a.css("margin-top"),10)||0)-q,u=a.outerHeight(!0),r=a.css("float"),h&&h.css({width:L(a),
+height:u,display:a.css("display"),"vertical-align":a.css("vertical-align"),"float":r}),e))return l()};x();if(u!==C)return D=void 0,d=q,z=E,l=function(){var c,l,e,k;if(!G&&(e=!1,null!=z&&(--z,0>=z&&(z=E,x(),e=!0)),e||A.height()===I||x(),e=f.scrollTop(),null!=D&&(l=e-D),D=e,m?(w&&(k=e+u+d>C+n,v&&!k&&(v=!1,a.css({position:"fixed",bottom:"",top:d}).trigger("sticky_kit:unbottom"))),e<F&&(m=!1,d=q,null==p&&("left"!==r&&"right"!==r||a.insertAfter(h),h.detach()),c={position:"",width:"",top:""},a.css(c).removeClass(t).trigger("sticky_kit:unstick")),
+B&&(c=f.height(),u+q>c&&!v&&(d-=l,d=Math.max(c-u,d),d=Math.min(q,d),m&&a.css({top:d+"px"})))):e>F&&(m=!0,c={position:"fixed",top:d},c.width="border-box"===a.css("box-sizing")?a.outerWidth()+"px":a.width()+"px",a.css(c).addClass(t),null==p&&(a.after(h),"left"!==r&&"right"!==r||h.append(a)),a.trigger("sticky_kit:stick")),m&&w&&(null==k&&(k=e+u+d>C+n),!v&&k)))return v=!0,"static"===g.css("position")&&g.css({position:"relative"}),a.css({position:"absolute",bottom:b,top:"auto"}).trigger("sticky_kit:bottom")},
+y=function(){x();return l()},H=function(){G=!0;f.off("touchmove",l);f.off("scroll",l);f.off("resize",y);c(document.body).off("sticky_kit:recalc",y);a.off("sticky_kit:detach",H);a.removeData("sticky_kit");a.css({position:"",bottom:"",top:"",width:""});g.position("position","");if(m)return null==p&&("left"!==r&&"right"!==r||a.insertAfter(h),h.remove()),a.removeClass(t)},f.on("touchmove",l),f.on("scroll",l),f.on("resize",y),c(document.body).on("sticky_kit:recalc",y),a.on("sticky_kit:detach",H),setTimeout(l,
+0)}};n=0;for(K=this.length;n<K;n++)b=this[n],J(c(b));return this}}).call(this);
